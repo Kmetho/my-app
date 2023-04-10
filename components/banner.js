8dfc4ff015e0656d2691ba6ccecc4e8cf1b170e4
Kmetho <94000429+Kmetho@users.noreply.github.com>
2023-04-10 21:56:55 +0200

    Update

--- components/banner.js
+++ components/banner.js
@@ -3,11 +3,11 @@ import styles from "./banner.module.css";
 const Banner = (props) => {
   return (
     <div className={styles.headerBox}>
-      <h1 className={styles.header}>Where I want to move</h1>
-      <h2 className={styles.underheader}>
-        A guide to help my undecisive ass decide
-      </h2>
-      <button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button>
+      <h1 className={styles.header}>Where to move</h1>
+      <h2 className={styles.underheader}>Nice locations</h2>
+      <button className={styles.button} onClick={props.handleOnClick}>
+        {props.buttonText}
+      </button>
     </div>
   );
 };
