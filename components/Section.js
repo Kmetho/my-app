2def50f4dd19db091d66065629dd9d9de88186da
Kmetho <94000429+Kmetho@users.noreply.github.com>
2023-06-18 13:32:08 +0200

    Update

--- components/Section.js
+++ components/Section.js
@@ -16,7 +16,7 @@ const Section = (props) => {
                 thisObject.imgUrl ||
                 "https://images.unsplash.com/photo-1503954230032-c850298b9df3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
               }
-              href={`/flower` + `/${thisObject.id}`}
+              href={`/place` + `/${thisObject.id}`}
             />
           );
         })}
